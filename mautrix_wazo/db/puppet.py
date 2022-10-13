66bae166ef6d3bd0d9a62e5498a82bea66a2000f
Jesse Sopel <jesse.sopel@wazo.io>
2022-10-13 17:40:19 -0400

    fix add columns for double puppeting

--- mautrix_wazo/db/puppet.py
+++ mautrix_wazo/db/puppet.py
@@ -4,8 +4,9 @@ from typing import ClassVar
 
 from asyncpg.protocol.protocol import Record
 from attr import dataclass
-from mautrix.types import UserID
+from mautrix.types import UserID, SyncToken
 from mautrix.util.async_db import Database
+from yarl import URL
 
 from mautrix_wazo.types import WazoUUID
 
@@ -20,23 +21,31 @@ class Puppet:
     username: str
     is_registered: bool
     custom_mxid: UserID | None
+    access_token: str | None
+    next_batch: SyncToken | None
+    base_url: URL | None
 
-    columns = 'wazo_uuid, first_name, last_name, username, is_registered, custom_mxid'
+    columns = 'wazo_uuid, first_name, last_name, username, is_registered, custom_mxid, access_token'
 
     async def insert(self) -> None:
         await self.db.execute(
-            f"INSERT INTO puppet ({self.columns}) VALUES ($1, $2, $3, $4, $5, $6)",
+            f"INSERT INTO puppet ({self.columns}) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9)",
             self.wazo_uuid,
             self.first_name,
             self.last_name,
             self.username,
             self.is_registered,
             self.custom_mxid,
+            self.access_token,
+            self.next_batch,
+            self.base_url,
         )
 
     async def update(self):
         await self.db.execute(
-            "UPDATE puppet SET first_name=$2, last_name=$3, username=$4, is_registered=$5, custom_mxid=$6 "
+            "UPDATE puppet SET "
+            "first_name=$2, last_name=$3, username=$4, is_registered=$5, custom_mxid=$6, access_token=$7, "
+            "next_batch=$8, base_url=$9"
             "WHERE wazo_uuid=$1",
             self.wazo_uuid,
             self.first_name,
@@ -44,6 +53,9 @@ class Puppet:
             self.username,
             self.is_registered,
             self.custom_mxid,
+            self.access_token,
+            self.next_batch,
+            self.base_url,
         )
 
     @classmethod
--- mautrix_wazo/db/upgrade.py
+++ mautrix_wazo/db/upgrade.py
@@ -4,7 +4,7 @@ from mautrix.util.async_db import UpgradeTable
 upgrade_table = UpgradeTable()
 
 
-@upgrade_table.register(description="Latest revision", upgrades_to=1)
+@upgrade_table.register(description="Latest revision", upgrades_to=2)
 async def upgrade_latest(conn: Connection) -> None:
     await conn.execute(
         """CREATE TABLE portal (
@@ -25,7 +25,10 @@ async def upgrade_latest(conn: Connection) -> None:
             last_name     TEXT,
             username      TEXT,
             is_registered BOOLEAN NOT NULL DEFAULT false,
-            custom_mxid  TEXT
+            custom_mxid  TEXT,
+            access_token TEXT,
+            next_batch   TEXT,
+            base_url     TEXT
         )"""
     )
     await conn.execute(
@@ -38,3 +41,10 @@ async def upgrade_latest(conn: Connection) -> None:
             timestamp BIGINT
         )"""
     )
+
+
+@upgrade_table.register(description="Add base_url, token and next_batch to puppet")
+async def upgrade_v2(conn: Connection) -> None:
+    await conn.execute("ALTER TABLE puppet ADD COLUMN access_token TEXT")
+    await conn.execute("ALTER TABLE puppet ADD COLUMN next_batch TEXT")
+    await conn.execute("ALTER TABLE puppet ADD COLUMN base_url TEXT")
